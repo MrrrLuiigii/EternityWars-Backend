8a19db9c9257bd9845bc1c28e8587c8428679bf2
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-02-18 12:00:54 +0100

    Create ShopHibernateContext.java

---
+++ Api/src/main/java/com/eternitywars/api/DAL/Contexts/Shop/ShopHibernateContext.java
@@ -0,0 +1,11 @@
+package com.eternitywars.api.DAL.Contexts.Shop;
+
+import com.eternitywars.api.Interfaces.Shop.IShopContext;
+import com.eternitywars.api.Models.User;
+
+public class ShopHibernateContext implements IShopContext {
+    @Override
+    public void BuyPack(User user) {
+
+    }
+}
