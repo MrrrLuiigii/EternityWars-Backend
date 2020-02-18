2eb32743ea552ac80c2c4ea78712f9ed5fbe34ef
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-02-18 12:00:51 +0100

    Create LobbyHibernateContext.java

---
+++ Api/src/main/java/com/eternitywars/api/DAL/Contexts/Lobby/LobbyHibernateContext.java
@@ -0,0 +1,27 @@
+package com.eternitywars.api.DAL.Contexts.Lobby;
+
+import com.eternitywars.api.Interfaces.Lobby.ILobbyContext;
+import com.eternitywars.api.Models.Lobby;
+import com.eternitywars.api.Models.Player;
+
+public class LobbyHibernateContext implements ILobbyContext {
+    @Override
+    public boolean JoinLobby(Lobby lobby, Player player) {
+        return false;
+    }
+
+    @Override
+    public boolean LeaveLobby(Lobby lobby, Player player) {
+        return false;
+    }
+
+    @Override
+    public boolean UpdatePlayerStatus(Lobby lobby, Player player) {
+        return false;
+    }
+
+    @Override
+    public Lobby UpdatePlayerDeck(Lobby lobby, Player player) {
+        return null;
+    }
+}
