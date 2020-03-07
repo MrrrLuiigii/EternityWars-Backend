8b73d4e2fb2812d82efbaacff4a4122f152077ad
nickyjjanse <i421118@fhict.nl>
2020-03-07 15:58:38 +0100

    home made bean for sessionfactory

--- Api/src/main/java/com/eternitywars/api/ApiApplication.java
+++ Api/src/main/java/com/eternitywars/api/ApiApplication.java
@@ -1,7 +1,15 @@
 package com.eternitywars.api;
 
+import com.eternitywars.api.Models.Entities.Card;
+import com.eternitywars.api.Models.Entities.Deck;
+import com.eternitywars.api.Models.Entities.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,4 +20,19 @@ public class ApiApplication
     {
         SpringApplication.run(ApiApplication.class, args);
     }
+
+    public static SessionFactory getSessionFactory()
+    {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+
+        configuration.addAnnotatedClass(User.class);
+        configuration.addAnnotatedClass(Card.class);
+        configuration.addAnnotatedClass(Deck.class);
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+
 }
