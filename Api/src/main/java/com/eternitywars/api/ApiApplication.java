e074ba6890ce983821261594bc7b5ba2e438ee6f
nickyjjanse <i421118@fhict.nl>
2020-03-07 17:29:10 +0100

    added entities to config

--- Api/src/main/java/com/eternitywars/api/ApiApplication.java
+++ Api/src/main/java/com/eternitywars/api/ApiApplication.java
@@ -1,15 +1,12 @@
 package com.eternitywars.api;
 
-import com.eternitywars.api.Models.Entities.Card;
-import com.eternitywars.api.Models.Entities.Deck;
-import com.eternitywars.api.Models.Entities.User;
+import com.eternitywars.api.Models.Entities.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -29,10 +26,13 @@ public class ApiApplication
         configuration.addAnnotatedClass(User.class);
         configuration.addAnnotatedClass(Card.class);
         configuration.addAnnotatedClass(Deck.class);
+        configuration.addAnnotatedClass(CardCollection.class);
+        configuration.addAnnotatedClass(DeckCard.class);
+        configuration.addAnnotatedClass(Relationship.class);
+        configuration.addAnnotatedClass(Lobby.class);
+        configuration.addAnnotatedClass(Player.class);
 
         ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
         return configuration.buildSessionFactory(serviceRegistry);
     }
-
-
 }
