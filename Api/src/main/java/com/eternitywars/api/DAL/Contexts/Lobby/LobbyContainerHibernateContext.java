e054b6f5aa6314b47e98b376d664e5495513bed4
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-03-09 12:05:13 +0100

    Update LobbyContainerHibernateContext.java

--- Api/src/main/java/com/eternitywars/api/DAL/Contexts/Lobby/LobbyContainerHibernateContext.java
+++ Api/src/main/java/com/eternitywars/api/DAL/Contexts/Lobby/LobbyContainerHibernateContext.java
@@ -1,63 +1,49 @@
 package com.eternitywars.api.DAL.Contexts.Lobby;
 
+import com.eternitywars.api.ApiApplication;
 import com.eternitywars.api.Interfaces.Lobby.ILobbyContainerContext;
 import com.eternitywars.api.Models.Lobbies;
 import com.eternitywars.api.Models.Entities.Lobby;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class LobbyContainerHibernateContext implements ILobbyContainerContext {
+
+    private static SessionFactory sessionFactory = ApiApplication.getSessionFactory();
+
+    private Session session;
+    private Transaction transaction;
+
     @Override
-    public Lobby AddLobby(Lobby lobby)
-    {
+    public Lobby AddLobby(Lobby lobby) {
         return null;
     }
 
     @Override
-    public boolean DeleteLobby(Lobby lobby)
-    {
+    public boolean DeleteLobby(Lobby lobby) {
         return false;
     }
 
     @Override
-    public Lobby GetLobbyById(int lobbyId)
-    {
-        return null;
+    public Lobby GetLobbyById(int lobbyId) {
+
+        Lobby returnlobby;
+
+        try {
+            session = sessionFactory.openSession();
+            returnlobby = session.find(Lobby.class, lobbyId);
+        } catch (Exception ex) {
+            return null;
+        } finally {
+            session.close();
+        }
+
+        return returnlobby;
     }
 
     @Override
-    public Lobbies GetLobbies()
-    {
+    public Lobbies GetLobbies() {
         return null;
     }
-//    private static EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("eternitywars");
-//    @Override
-//    public Lobby AddLobby(Lobby lobby) {
-//        return null;
-//    }
-//
-//    @Override
-//    public boolean DeleteLobby(Lobby lobby) {
-//        return false;
-//    }
-//
-//    @Override
-//    public Lobby GetLobbyById(int lobbyId) {
-//        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
-//
-//        Lobby returnlobby;
-//
-//        try {
-//            returnlobby = entityManager.find(Lobby.class, lobbyId);
-//        } catch (Exception ex) {
-//            return null;
-//        } finally {
-//            entityManager.close();
-//        }
-//
-//        return returnlobby;
-//    }
-//
-//    @Override
-//    public Lobbies GetLobbies() {
-//        return null;
-//    }
 }
