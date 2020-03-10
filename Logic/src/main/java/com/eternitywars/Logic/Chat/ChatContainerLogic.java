492295150918dd07cd00755de4e6cca39c516d3d
wiebj3 <wiebj3@gmail.com>
2020-03-10 11:26:46 +0100

    Methods made for ChatLogic,ChatContainerLogic

--- Logic/src/main/java/com/eternitywars/Logic/Chat/ChatContainerLogic.java
+++ Logic/src/main/java/com/eternitywars/Logic/Chat/ChatContainerLogic.java
@@ -1,29 +1,38 @@
 package com.eternitywars.Logic.Chat;
 
+import com.eternitywars.Logic.utils.APIRequest;
 import com.eternitywars.Models.Chat;
 import com.eternitywars.Models.ChatCollection;
 import com.google.gson.JsonObject;
+import org.springframework.http.HttpMethod;
 
 public class ChatContainerLogic
 {
     public ChatCollection GetAllChats(JsonObject jsonObject)
     {
-        return null;
+        ChatCollection chatCollection = (ChatCollection) APIRequest.CreateRequest("http://localhost:8083/api/private/chat/getallchats", jsonObject.toString(), ChatCollection.class, HttpMethod.GET);
+        return chatCollection;
     }
 
     public Chat GetChatById(JsonObject jsonObject)
     {
-        return null;
+        Chat chat = (Chat) APIRequest.CreateRequest("http://localhost:8083/api/private/chat/getchatbyid", jsonObject.toString(), Chat.class, HttpMethod.GET);
+        return chat;
     }
 
     public Boolean DeleteChat(JsonObject jsonObject)
     {
-        return true;
+        if(APIRequest.CreateRequest("http://localhost:8083/api/private/chat/deletechat", jsonObject.toString(), Chat.class, HttpMethod.DELETE) != null)
+        {
+            return true;
+        }
+        return false;
     }
 
     public Chat CreateChat(JsonObject jsonObject)
     {
-        return null;
+        Chat chat = (Chat) APIRequest.CreateRequest("http://localhost:8083/api/private/chat/createchat", jsonObject.toString(), Chat.class, HttpMethod.POST);
+        return chat;
     }
 
 
