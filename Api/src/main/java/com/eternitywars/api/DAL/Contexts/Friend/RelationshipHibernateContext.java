70a120d485ea9ae9922643dca070605e6ef87c85
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2020-03-11 14:33:13 +0100

    implemented update friend

--- Api/src/main/java/com/eternitywars/api/DAL/Contexts/Friend/RelationshipHibernateContext.java
+++ Api/src/main/java/com/eternitywars/api/DAL/Contexts/Friend/RelationshipHibernateContext.java
@@ -1,11 +1,40 @@
 package com.eternitywars.api.DAL.Contexts.Friend;
 
+import com.eternitywars.api.ApiApplication;
 import com.eternitywars.api.Interfaces.Friend.IRelationshipContext;
 import com.eternitywars.api.Models.Entities.Relationship;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
-public class RelationshipHibernateContext implements IRelationshipContext {
+public class RelationshipHibernateContext implements IRelationshipContext
+{
+    private static SessionFactory sessionFactory = ApiApplication.getSessionFactory();
+
+    private Session session;
+    private Transaction transaction;
+
     @Override
-    public boolean UpdateRelationship(Relationship relationship) {
-        return false;
+    public boolean UpdateRelationship(Relationship relationship)
+    {
+        try
+        {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(relationship);
+            transaction.commit();
+        } catch (Exception ex)
+        {
+            if (transaction != null)
+            {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        } finally
+        {
+            session.close();
+        }
+
+        return true;
     }
 }
