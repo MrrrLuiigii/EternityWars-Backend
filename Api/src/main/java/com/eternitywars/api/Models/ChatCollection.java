21e42f69dba100a21b1087e429db65df7abf106e
nickyjjanse <i421118@fhict.nl>
2020-03-17 10:10:06 +0100

    format

--- Api/src/main/java/com/eternitywars/api/Models/ChatCollection.java
+++ Api/src/main/java/com/eternitywars/api/Models/ChatCollection.java
@@ -1,23 +1,29 @@
 package com.eternitywars.api.Models;
 
 import com.eternitywars.api.Models.Entities.Chat;
+
 import java.util.List;
 
-public class ChatCollection {
+public class ChatCollection
+{
     List<Chat> chats;
 
-    public ChatCollection() {
+    public ChatCollection()
+    {
     }
 
-    public ChatCollection(List<Chat> chats) {
+    public ChatCollection(List<Chat> chats)
+    {
         this.chats = chats;
     }
 
-    public List<Chat> getChats() {
+    public List<Chat> getChats()
+    {
         return chats;
     }
 
-    public void setChats(List<Chat> chats) {
+    public void setChats(List<Chat> chats)
+    {
         this.chats = chats;
     }
 }
