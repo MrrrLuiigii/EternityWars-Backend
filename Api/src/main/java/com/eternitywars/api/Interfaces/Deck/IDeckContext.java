f490cc0266ea3b3aadec212a832190e5244737bc
nickyjjanse <i421118@fhict.nl>
2020-03-30 15:36:02 +0200

    remove card from call (already in deck)

--- Api/src/main/java/com/eternitywars/api/Interfaces/Deck/IDeckContext.java
+++ Api/src/main/java/com/eternitywars/api/Interfaces/Deck/IDeckContext.java
@@ -1,13 +1,12 @@
 package com.eternitywars.api.Interfaces.Deck;
 
-import com.eternitywars.api.Models.Entities.Card;
 import com.eternitywars.api.Models.Entities.Deck;
 
 public interface IDeckContext
 {
-    boolean AddCard(Deck deck, Card card);
+    boolean AddCard(Deck deck);
 
-    boolean DeleteCard(Deck deck, Card card);
+    boolean DeleteCard(Deck deck);
 
     boolean UpdateDeckName(Deck deck);
 }
