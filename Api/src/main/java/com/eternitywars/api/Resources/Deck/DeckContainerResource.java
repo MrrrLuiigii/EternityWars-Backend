0765c23829b7f49282e45ed58211a9f1f0b0e796
nickyjjanse <i421118@fhict.nl>
2020-03-30 14:41:16 +0200

    private -> public

--- Api/src/main/java/com/eternitywars/api/Resources/Deck/DeckContainerResource.java
+++ Api/src/main/java/com/eternitywars/api/Resources/Deck/DeckContainerResource.java
@@ -2,12 +2,12 @@ package com.eternitywars.api.Resources.Deck;
 
 
 import com.eternitywars.api.DAL.Repositories.Deck.DeckContainerRepository;
-import com.eternitywars.api.Models.Entities.Deck;
 import com.eternitywars.api.Models.Decks;
+import com.eternitywars.api.Models.Entities.Deck;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/private/deck")
+@RequestMapping(value = "/api/public/deck")
 public class DeckContainerResource
 {
     private DeckContainerRepository deckContainerRepository = new DeckContainerRepository();
