ffe3102dc92d9227a6fc92de48e7764840b77021
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-03-31 10:08:35 +0200

    getuserbyemail viewmodel

--- Logic/src/main/java/com/eternitywars/Models/CardCollection.java
+++ Logic/src/main/java/com/eternitywars/Models/CardCollection.java
@@ -21,4 +21,5 @@ public class CardCollection
     {
         this.cards = cards;
     }
+
 }
---
+++ Logic/src/main/java/com/eternitywars/Models/Viewmodels/SingleUserViewmodel.java
@@ -0,0 +1,88 @@
+package com.eternitywars.Models.Viewmodels;
+
+import com.eternitywars.Models.Enums.AccountStatus;
+
+    public class SingleUserViewmodel
+    {
+        private int id;
+        private String username;
+        private String email;
+        private AccountStatus status;
+        private int gold;
+        private int packAmount;
+
+        public SingleUserViewmodel()
+        {
+        }
+
+        public SingleUserViewmodel(int id, String username, String email, AccountStatus status, int gold, int packAmount)
+        {
+            this.id = id;
+            this.username = username;
+            this.email = email;
+            this.status = status;
+            this.gold = gold;
+            this.packAmount = packAmount;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public String getUsername()
+        {
+            return username;
+        }
+
+        public void setUsername(String username)
+        {
+            this.username = username;
+        }
+
+        public String getEmail()
+        {
+            return email;
+        }
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+
+        public AccountStatus getStatus()
+        {
+            return status;
+        }
+
+        public void setStatus(AccountStatus status)
+        {
+            this.status = status;
+        }
+
+        public int getGold()
+        {
+            return gold;
+        }
+
+        public void setGold(int gold)
+        {
+            this.gold = gold;
+        }
+
+        public int getPackAmount()
+        {
+            return packAmount;
+        }
+
+        public void setPackAmount(int packAmount)
+        {
+            this.packAmount = packAmount;
+        }
+    }
+
