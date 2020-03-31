faebf8af460b21ae2f32a2a498fce3a41473710f
nickyjjanse <i421118@fhict.nl>
2020-03-31 11:20:41 +0200

    add/delete card from deck

--- Api/src/main/java/com/eternitywars/api/DAL/Contexts/Deck/DeckHibernateContext.java
+++ Api/src/main/java/com/eternitywars/api/DAL/Contexts/Deck/DeckHibernateContext.java
@@ -2,6 +2,7 @@ package com.eternitywars.api.DAL.Contexts.Deck;
 
 import com.eternitywars.api.ApiApplication;
 import com.eternitywars.api.Interfaces.Deck.IDeckContext;
+import com.eternitywars.api.Models.Entities.Card;
 import com.eternitywars.api.Models.Entities.Deck;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -18,18 +19,102 @@ public class DeckHibernateContext implements IDeckContext
     @Override
     public boolean AddCard(Deck deck)
     {
-        return false;
+        boolean status = true;
+
+        try
+        {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            Card card = deck.getCards().get(0);
+            Deck getDeck = session.find(Deck.class, deck.getDeckId());
+            getDeck.getCards().add(card);
+            session.merge(getDeck);
+            transaction.commit();
+        } catch (Exception ex)
+        {
+            if (transaction != null)
+            {
+                transaction.rollback();
+            }
+
+            status = false;
+            ex.printStackTrace();
+        } finally
+        {
+            session.close();
+        }
+
+        return status;
     }
 
     @Override
     public boolean DeleteCard(Deck deck)
     {
-        return false;
+        boolean status = true;
+
+        try
+        {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            Card card = deck.getCards().get(0);
+            Deck getDeck = session.find(Deck.class, deck.getDeckId());
+
+            for (Card c : getDeck.getCards())
+            {
+                if (c.getCardId() == card.getCardId())
+                {
+                    getDeck.getCards().remove(c);
+                }
+            }
+
+            session.merge(getDeck);
+            transaction.commit();
+        } catch (Exception ex)
+        {
+            if (transaction != null)
+            {
+                transaction.rollback();
+            }
+
+            status = false;
+            ex.printStackTrace();
+        } finally
+        {
+            session.close();
+        }
+
+        return status;
     }
 
     @Override
     public boolean UpdateDeckName(Deck deck)
     {
-        return false;
+        boolean status = true;
+
+        try
+        {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+            String name = deck.getName();
+            Deck getDeck = session.find(Deck.class, deck.getDeckId());
+            getDeck.setName(name);
+            session.merge(getDeck);
+            transaction.commit();
+        } catch (Exception ex)
+        {
+            if (transaction != null)
+            {
+                transaction.rollback();
+            }
+
+            status = false;
+            ex.printStackTrace();
+        } finally
+        {
+            session.close();
+        }
+
+        return status;
     }
 }
