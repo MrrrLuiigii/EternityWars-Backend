cfabf8277124b302c4c36a0c138bf34402a08ad4
nickyjjanse <i421118@fhict.nl>
2020-04-07 14:01:27 +0200

    rm import

--- Api/src/main/java/com/eternitywars/api/Models/Entities/User.java
+++ Api/src/main/java/com/eternitywars/api/Models/Entities/User.java
@@ -5,7 +5,6 @@ import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
